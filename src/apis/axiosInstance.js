728c09138e219dd7efc8bc1778f128d7439e5c83
Kapil Paliwal <paliwalkapil2004@gmail.com>
2024-08-11 17:13:45 +0530

    update backend

--- src/apis/axiosInstance.js
+++ src/apis/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an instance of Axios with a base URL
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000/v1/api", // Base URL for API requests
+  baseURL: "https://expense-tracker-durgesh-paliwal-v1.onrender.com/v1/api", // Base URL for API requests
 });
 
 axiosInstance.interceptors.request.use(
